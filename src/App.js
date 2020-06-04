9d826632b8686ddf05f4e0df0f92e35d11cbcc76
losrobbos <losrobbos@web.de>
2020-06-04 13:10:39 +0200

    Added router with two pages

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
-
   state = {
     todos: []
-  }
+  };
 
   componentDidMount() {
-
     // process.env holds all our variables (key value pairs) stored in .env file
-    let API_URL = process.env.REACT_APP_API_URL
+    let API_URL = process.env.REACT_APP_API_URL;
 
-    console.log("API URL", API_URL)
+    console.log('API URL', API_URL);
 
-    fetch(API_URL)
-    .then(res => res.json())
-    .then(todosApi => {
-      this.setState({todos: todosApi})
-    })
-
+    fetch(API_URL).then((res) => res.json()).then((todosApi) => {
+      this.setState({ todos: todosApi });
+    });
   }
 
   render() {
+    let todos = this.state.todos;
 
-    let todos = this.state.todos
-
-    let jsxTodoList = todos.map(todo => (
-      <div key={todo._id}>{todo.text}</div>
-    ))
+    let jsxTodoList = todos.map((todo) => <div key={todo._id}>{todo.text}</div>);
 
     return (
       <div className="App">
-        <header className="App-header">
-          {todos.length > 0 && jsxTodoList }
-        </header>
+        <Switch>
+          <Route path="/" exact>
+            <header className="App-header">
+              {todos.length > 0 && jsxTodoList}
+            </header>
+          </Route>
+          <Route path="/other">
+            <div>
+              <h1>Other page</h1>
+            </div>
+          </Route>
+        </Switch>
       </div>
     );
   }
